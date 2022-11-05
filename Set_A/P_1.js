e08696d863ae2069d5caae2891cbd9c41395d949
Prathmesh <connect.prathmesh905@gmail.com>
2022-11-05 18:55:05 +0530

    updated the title of the question

--- Set_A/P_1.js
+++ Set_A/P_1.js
@@ -1,4 +1,4 @@
-/*Problem 1-  
+/*Problem 1-  (From codechef)
 Chef took an examination two times. 
 In the first attempt, he scored XX marks while in the second attempt he scored YY marks. 
 According to the rules of the examination, the best score out of the two attempts will be considered as the final score.*/
