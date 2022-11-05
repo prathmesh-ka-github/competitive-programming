bb26e9e739229d73522ab11cb9aa45a0a7fa2a2c
Prathmesh <connect.prathmesh905@gmail.com>
2022-11-05 18:51:32 +0530

    solved the first question!

--- Set_A/P_1.js
+++ Set_A/P_1.js
@@ -1,4 +1,27 @@
-// Problem 1-  
-// Chef took an examination two times. 
-//In the first attempt, he scored XX marks while in the second attempt he scored YY marks. 
-//According to the rules of the examination, the best score out of the two attempts will be considered as the final score.
+/*Problem 1-  
+Chef took an examination two times. 
+In the first attempt, he scored XX marks while in the second attempt he scored YY marks. 
+According to the rules of the examination, the best score out of the two attempts will be considered as the final score.*/
+
+console.log("\n\n\n                Problem number 1\n");
+
+const readline = require("readline");
+
+const input1 = readline.createInterface(
+    {input: process.stdin, 
+    output: process.stdout,}); //function with 2 arguments (input, output)
+
+input1.question("Scores of first attempt - ",
+    (answer1) => {
+        input1.question("Scores of second attempt - ", 
+        (answer2)=> {
+            if (+answer1 > +answer2)
+                console.log(`Best score of them both is - ${answer1}`)
+            else
+                console.log(`Best score of them both is - ${answer2}`)
+            
+            console.log('\n\n\n')
+            input1.close()
+        })
+    }
+);
