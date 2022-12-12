f1d15328c8aa3ba1ff24d82740c67174ba012fde
Prathmesh Kale <connect.prathmesh905@gmail.com>
2022-12-12 16:57:20 +0530

    p6 code chef

---
+++ CodeChef/P_6.py
@@ -0,0 +1,11 @@
+# Bull or Bear
+
+# Chef is on his way to become the new big bull of the stock market 
+# but is a bit weak at calculating whether he made a profit or a loss on his deal.
+# Given that Chef bought the stock at value XX and sold it at value YY. 
+# Help him calculate whether he made a profit, loss, or was it a neutral deal.
+
+# Input Format
+# The first line of input will contain a single integer TT, denoting the number of test cases.
+# Each test case consists of a single line of input containing two space-separated integers XX and YY, 
+# denoting the value at which Chef bought and sold the stock respectively.
